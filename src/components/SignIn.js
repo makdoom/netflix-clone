3c9f61097fccf49a88e6b5fff93de266c5def851
Makdoom Shaikh <makshaikh99@gmail.com>
2020-08-03 16:45:45 +0530

    Completed sign in page

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,7 +1,46 @@
 import React from "react";
+import "./Signin.css";
 
 const Signup = () => {
-  return <div>Hello SignIn page goes here</div>;
+  return (
+    <div className="signin__page">
+      <div className="header">
+        <img
+          src="https://www.freepnglogos.com/uploads/netflix-logo-0.png"
+          alt="Netflix Logo"
+        />
+      </div>
+      <div className="signin__box">
+        <div className="heading">
+          <h2>Sign In</h2>
+        </div>
+        <div className="input__box">
+          <form>
+            <input className="input" type="text" placeholder="Username" />
+            <br />
+            <input className="input" type="email" placeholder="Email" />
+            <br />
+            <input
+              className="input"
+              type="password"
+              placeholder="New Password"
+            />
+            <br />
+            <button className="submit">Submit</button>
+          </form>
+          <div className="help">
+            <div className="rem">
+              <input type="checkbox" name="remember" value="remember" />
+              <label for="remember">Remember me</label>
+            </div>
+            <div className="need">
+              <a href="#">Need help ?</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
