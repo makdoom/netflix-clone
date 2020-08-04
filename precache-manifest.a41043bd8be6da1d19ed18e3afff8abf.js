aca4939df15c9f34d17c8477d6e101a8461e3629
Makdoom Shaikh <makshaikh99@gmail.com>
2020-08-04 14:35:25 +0530

    Updates

---
+++ precache-manifest.a41043bd8be6da1d19ed18e3afff8abf.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a94cbabcb5d3156e5a282c1fbdb9fbf",
+    "url": "/netflix-clone/index.html"
+  },
+  {
+    "revision": "05d3cec59f83707953d0",
+    "url": "/netflix-clone/static/css/2.a516ef31.chunk.css"
+  },
+  {
+    "revision": "e47f62af8d9d35ae7193",
+    "url": "/netflix-clone/static/css/main.f3423f33.chunk.css"
+  },
+  {
+    "revision": "05d3cec59f83707953d0",
+    "url": "/netflix-clone/static/js/2.3c6ba829.chunk.js"
+  },
+  {
+    "revision": "8a9adb48f7af2062183ac70b4922c4ef",
+    "url": "/netflix-clone/static/js/2.3c6ba829.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e47f62af8d9d35ae7193",
+    "url": "/netflix-clone/static/js/main.74d34148.chunk.js"
+  },
+  {
+    "revision": "42ccba1c2e183f5632cb",
+    "url": "/netflix-clone/static/js/runtime-main.baddefd0.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/netflix-clone/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/netflix-clone/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/netflix-clone/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/netflix-clone/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/netflix-clone/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
